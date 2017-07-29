abe526e48cb4a503862dfd5ab376fdab36119397
semsyamsidin <samsidin.sem@gmail>
2017-07-29 21:24:33 +0700

    pedagang

--- app/src/main/java/com/syamsidin/homepedagang/fragment/PenilaianFragment.java
+++ app/src/main/java/com/syamsidin/homepedagang/fragment/PenilaianFragment.java
@@ -19,6 +19,7 @@ import com.syamsidin.homepedagang.adapter.AdapterPenilaian;
 
 public class PenilaianFragment extends Fragment {
 
+
     private static final String TAG = "RecyclerViewFragment";
     private static final String KEY_LAYOUT_MANAGER = "layoutManager";
     private static final int SPAN_COUNT = 2;
